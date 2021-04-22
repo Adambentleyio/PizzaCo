52dcb45478ac329634e1184d7b4f685b71aad4d8
Adam <bentleyadamj@gmail.com>
2021-04-22 12:23:24 +0100

    Added Order Page including utility functions for adding r

--- gatsby/src/pages/index.js
+++ gatsby/src/pages/index.js
@@ -1,9 +1,25 @@
 import React from 'react';
+import useLatestData from '../utils/useLatestData';
+import { HomePageGrid } from '../styles/Grids';
+
+function CurrentlySlicing({ slicemasters }) {
+  return <h3> Slicing</h3>;
+}
+
+function HotSlices() {
+  return <h3> Hot Slices</h3>;
+}
 
 export default function HomePage() {
+  const { slicemasters, hotSlices } = useLatestData();
   return (
-    <div>
-      <p>Hey, I'm the homepage!</p>
+    <div className="center">
+      <h1>The Best Pizza Downtown</h1>
+      <p>Open 11am to 11pm</p>
+      <HomePageGrid>
+        <CurrentlySlicing slicemasters={slicemasters} />
+        <HotSlices hotSlices={hotSlices} />
+      </HomePageGrid>
     </div>
   );
 }
--- gatsby/src/pages/order.js
+++ gatsby/src/pages/order.js
@@ -1,11 +1,138 @@
 import React from 'react';
+import { graphql } from 'gatsby';
+import Img from 'gatsby-image';
+import useForm from '../utils/useForm';
 import SEO from '../components/SEO';
+import calculatePizzaPrice from '../utils/calculatePizzaPrice';
+import formatMoney from '../utils/formatMoney';
+import OrderStyles from '../styles/OrderStyles';
+import MenuStyles from '../styles/MenuItemStyles';
+import usePizza from '../utils/usePizza';
+import PizzaOrder from '../components/PizzaOrder';
+import calculateOrderTotal from '../utils/calculateOrderTotal';
 
-export default function OrderPage() {
+export default function OrderPage({ data }) {
+  const pizzas = data.pizzas.nodes;
+
+  const { values, updateValue } = useForm({
+    name: '',
+    email: '',
+    treacle: '',
+  });
+
+  const {
+    order,
+    addToOrder,
+    removeFromOrder,
+    error,
+    loading,
+    message,
+    submitOrder,
+  } = usePizza({
+    pizzas,
+    values,
+  });
+
+  if (message) {
+    return <p>{message}</p>;
+  }
+
   return (
     <>
       <SEO title="Order a Pizza!" />
-      <p> I am the order page</p>
+      <OrderStyles onSubmit={submitOrder}>
+        <fieldset>
+          <legend>Your Info</legend>
+          <label htmlFor="name">Name</label>
+          <input
+            type="text"
+            name="name"
+            value={values.name}
+            onChange={updateValue}
+          />
+          <label htmlFor="email">Email</label>
+          <input
+            type="email"
+            name="email"
+            value={values.email}
+            onChange={updateValue}
+          />
+          <input
+            type="treacle"
+            name="treacle"
+            id="treacle"
+            className="treacle"
+            value={values.treacle}
+            onChange={updateValue}
+          />
+        </fieldset>
+        <fieldset className="menu">
+          <legend>Menu</legend>
+          {pizzas.map((pizza) => (
+            <MenuStyles key={pizza.id}>
+              <Img width="50" height="50" fluid={pizza.image.asset.fluid} />
+              <div>
+                <h2>{pizza.name}</h2>
+                {['S', 'M', 'L'].map((size) => (
+                  <button
+                    type="button"
+                    key={size}
+                    onClick={() =>
+                      addToOrder({
+                        id: pizza.id,
+                        size,
+                      })
+                    }
+                  >
+                    {size} {formatMoney(calculatePizzaPrice(pizza.price, size))}
+                  </button>
+                ))}
+              </div>
+            </MenuStyles>
+          ))}
+        </fieldset>
+        <fieldset className="order">
+          <legend>Order</legend>
+          <PizzaOrder
+            order={order}
+            removeFromOrder={removeFromOrder}
+            pizzas={pizzas}
+          />
+        </fieldset>
+        <fieldset>
+          <h3>
+            Your Total is {formatMoney(calculateOrderTotal(order, pizzas))}
+          </h3>
+          <div aria-live="polite" aria-atomic="true">
+            {error ? <p>Error: {error}</p> : ''}
+          </div>
+          <button type="submit" disabled={loading}>
+            {loading ? 'Placing Order...' : 'Order Ahead'}
+          </button>
+        </fieldset>
+      </OrderStyles>
     </>
   );
 }
+
+export const query = graphql`
+  query {
+    pizzas: allSanityPizza {
+      nodes {
+        name
+        id
+        slug {
+          current
+        }
+        price
+        image {
+          asset {
+            fluid(maxWidth: 100) {
+              ...GatsbySanityImageFluid
+            }
+          }
+        }
+      }
+    }
+  }
+`;
